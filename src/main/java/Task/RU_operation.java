221e793999825990f24bc77184b0839f9670d6ce
bharathi-selvakumar <bharathiselvakumar286@gmail.com>
2024-07-10 11:37:22 +0530

    curd

--- src/main/java/Task/RU_operation.java
+++ src/main/java/Task/RU_operation.java
@@ -29,16 +29,16 @@ public class RU_operation {
                     System.out.println("Bank balance :" + Arrays.toString(balance));
                     float persentage = (float) 3 / 100;
                     System.out.println(persentage);
-                    float cal = (balance[position] * persentage);
-                    System.out.println(cal);
-                    balance[position] = balance[position] - (int) cal;
+                    float cal_ = (balance[position] * persentage);
+                    System.out.println(cal_);
+                    balance[position] = balance[position] - (int) cal_;
                     System.out.println("the position " + position + " is updated,where it have the amount of below 10000 " + "[" + balance[position] + "]");
                     System.out.println("Bank balance :" + Arrays.toString(balance));
-                    float persentage = (float) 5 / 100;
-                    System.out.println(persentage);
-                    float cal = (balance[position] * persentage);
-                    System.out.println(cal);
-                    balance[position] = balance[position] - (int) cal;
+                    float persentage_2 = (float) 5 / 100;
+                    System.out.println(persentage_2);
+                    float cal_new = (balance[position] * persentage_2);
+                    System.out.println(cal_new);
+                    balance[position] = balance[position] - (int) cal_new;
                     System.out.println("the position " + position + " is updated is updated,where it have the amount of below 5000 :" + "[" + balance[position] + "]");
                     return "The index values that you choose,That have amount greater than 10,000 ";
                 }
